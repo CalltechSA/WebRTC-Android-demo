048deccd543f402f3895c22be0c09d2f4f288f1d
desarrollo1Calltech <desarrollo1@calltechsa.com>
2020-01-02 10:16:50 -0500

    first commit

---
+++ app/src/main/java/com/diegofn/webrtc_demo/MainActivity.java
@@ -0,0 +1,70 @@
+package com.diegofn.webrtc_demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.PermissionRequest;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+    private PermissionRequest mPermissionRequest;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://clicktocall.americasbps.com/Click_to_call/");
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        webView.setWebChromeClient(new WebChromeClient() {
+
+            // This method is called when the web content is requesting permission to access some
+            // resources.
+            @Override
+            public void onPermissionRequest(PermissionRequest request) {
+                mPermissionRequest = request;
+                final String[] requestedResources = request.getResources();
+                for (String r : requestedResources) {
+                    if (r.equals(PermissionRequest.RESOURCE_AUDIO_CAPTURE)) {
+                        if(request.getOrigin().toString().equals("https://clicktocall.americasbps.com/")) {
+                            request.grant(request.getResources());
+                        } else {
+                            request.deny();
+                        }
+                    }
+                }
+            }
+
+        });
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        /**
+         * When the application falls into the background we want to stop the media stream
+         * such that the camera is free to use by other apps.
+         */
+        webView.evaluateJavascript("if(window.localStream){window.localStream.stop();}", null);
+    }
+}
