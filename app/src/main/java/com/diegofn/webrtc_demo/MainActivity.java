f4802bb287f3b0043f1badea9cdd069f81ec9cc7
desarrollo1Calltech <desarrollo1@calltechsa.com>
2020-01-29 15:10:33 -0500

    Audio Output solved

--- app/src/main/java/com/diegofn/webrtc_demo/MainActivity.java
+++ app/src/main/java/com/diegofn/webrtc_demo/MainActivity.java
@@ -2,9 +2,13 @@ package com.diegofn.webrtc_demo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.pm.ApplicationInfo;
+import android.media.AudioManager;
+import android.net.http.SslError;
 import android.os.Bundle;
 
+import android.webkit.SslErrorHandler;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -14,6 +18,9 @@ import android.webkit.WebViewClient;
 public class MainActivity extends AppCompatActivity {
     private WebView webView;
     private PermissionRequest mPermissionRequest;
+    private AudioManager audioManager;
+    private static final String DESKTOP_USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,12 +30,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+/*
+        audioManager = (AudioManager)getApplicationContext().getSystemService(Context.AUDIO_SERVICE);
+        audioManager.setMode(AudioManager.MODE_NORMAL);
+        audioManager.setSpeakerphoneOn(false);
+*/
+
         webView = findViewById(R.id.webview);
         webView.setWebViewClient(new WebViewClient());
-        webView.loadUrl("https://demos.calltechsa.com:444/CTWeb/");
-
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
+        webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setUseWideViewPort(true);
+        webSettings.setLoadWithOverviewMode(true);
+        webSettings.setMediaPlaybackRequiresUserGesture(false);
+        webSettings.setUserAgentString(DESKTOP_USER_AGENT);
+
+
+        webView.setWebViewClient(
+                new SSLTolerentWebViewClient()
+        );
+        webView.loadUrl("https://clicktocall.calltechsa.com/clicktovideo/clicktovideo.html");
 
         webView.setWebChromeClient(new WebChromeClient() {
 
@@ -40,17 +63,25 @@ public class MainActivity extends AppCompatActivity {
                 final String[] requestedResources = request.getResources();
                 for (String r : requestedResources) {
                     if (r.equals(PermissionRequest.RESOURCE_AUDIO_CAPTURE)) {
-                        if(request.getOrigin().toString().equals("https://demos.calltechsa.com:444/")) {
+                        if(request.getOrigin().toString().equals("https://clicktocall.calltechsa.com/")) {
                             request.grant(request.getResources());
+
                         } else {
                             request.deny();
                         }
                     }
                 }
             }
-
         });
 
+    }
+
+     //https://stackoverflow.com/questions/48702140/webview-https-handshake-failed
+    class SSLTolerentWebViewClient extends WebViewClient {
+        @Override
+        public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+            handler.proceed(); // Ignore SSL certificate errors
+        }
     }
 
     @Override
